c59c7f77457b37209f919011c43711af07c36c65
AlexHdz0001 <50603384+AlexHdz0001@users.noreply.github.com>
2019-06-06 11:52:36 -0500

    Add files via upload

---
+++ CentroDeAdopcioon/CentroDeAdopcioon/Adoptar.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CentroDeAdopcioon
+{
+    class Adoptar
+    {
+        private long NumeroAdopcion;
+        private string Mascota;
+        private double Peso;
+        private double Altura;
+        private string Nombre;
+
+        public Adoptar(long numeroAdopcion, string mascota, double peso, double altura, string nombre)
+        {
+            this.NumeroAdopcion = numeroAdopcion;
+            this.Mascota = mascota;
+            this.Peso = peso;
+            this.Altura = altura;
+            this.Nombre = nombre;
+        }
+
+        public void SetNumeroAdopcion(long numeroAdopcion)
+        {
+            this.NumeroAdopcion = numeroAdopcion;
+        }
+
+        public void SetMascota(string mascota)
+        {
+            this.Mascota = mascota;
+        }
+
+        public void SetPeso(double peso)
+        {
+            this.Peso = peso;
+        }
+
+        public void SetAltura(double altura)
+        {
+            this.Altura = altura;
+        }
+
+        public void SetNombre(string nombre)
+        {
+            this.Nombre = nombre;
+        }
+
+        public long GetNuneroAdopcion()
+        {
+            return NumeroAdopcion;
+        }
+
+        public string GetMascota()
+        {
+            return Mascota;
+        }
+
+        public double GetPeso()
+        {
+            return Peso;
+        }
+
+        public double GetAltura()
+        {
+            return Altura;
+        }
+
+        public string GetNombre()
+        {
+            return Nombre;
+        }
+
+    }
+}
---
+++ CentroDeAdopcioon/CentroDeAdopcioon/CentroDeAdopcion.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CentroDeAdopcioon
+{
+    class CentroDeAdopcion
+    {
+        static void Main(string[] args)
+        {
+            
+        Boolean Salida = false;
+
+
+        List<Adoptar> adoptars = new List<Adoptar>();
+
+            while(Salida == false)
+            {
+
+                Console.WriteLine("Ingresa el numero de adopcion");
+                long Num = Byte.Parse(Console.ReadLine());
+
+                Console.WriteLine("Ingresa el Tipo de Mascota");
+                Console.WriteLine("Perro");
+                Console.WriteLine("Gato");
+                Console.WriteLine("Otro");
+                String mas = Console.ReadLine();
+
+                Console.WriteLine("Peso (solo medida en kilogramos con punto decimal)");
+                double peso = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Altura (metros con punto decimal)");
+                double alt = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Nombre del animal");
+                string nombre = Console.ReadLine();
+
+                adoptars.Add(new Adoptar(Num,mas,peso,alt,nombre));
+
+                for (int I = 0; I < adoptars.Count; I++)
+                {
+                    long na = adoptars[I].GetNuneroAdopcion();
+                    string m = adoptars[I].GetMascota();
+                    double p = adoptars[I].GetPeso();
+                    double a = adoptars[I].GetAltura();
+                    string n = adoptars[I].GetNombre();
+
+                    Console.WriteLine("Numero de Adopcion: "+ na + ", Tipo de mascota: " + m + ", Peso: " + p + ", Altura: " +a+", Nombre de la mascota: "+n);
+                    
+
+
+                }
+
+                string respuesta;
+                Console.WriteLine("¿Desea Salir?");
+                respuesta = Console.ReadLine();
+                if (respuesta == "s" || respuesta == "si" || respuesta == "Si" || respuesta == "SI" || respuesta == "sI")
+                {
+                    Environment.Exit(1);
+                }
+                else
+                {
+                    Console.WriteLine("Muy bien sigamos");
+                }
+            }
+
+        }
+    }
+}
